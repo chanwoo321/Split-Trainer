248553a210c18bb44d1b04a1f2909eac8cf5dd14
Jonathan Kim <47134352+chanwoo321@users.noreply.github.com>
2021-01-01 02:34:06 -0600

    Add files via upload
    Adding initial files

---
+++ practice.js
@@ -0,0 +1,4 @@
+// JavaScript Document
+// This is the script needed to generate the practice mode where a metronome can be adjusted/played.
+// Additionally, the space bar is used in order to generate a sound that is identical to the met.
+
